7e1f1253b26addae8f7287b1f6a8fcd027fef808
gabriel1324t <gabriel.ignacio.carrasco1325@gmail.com>
2025-09-01 13:05:14 -0300

    Update homeScreen.js

--- src/homeScreen.js
+++ src/homeScreen.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { View, Button } from "react-native";
 import {useNavigation} from "@react-navigation/native";
+import { useState } from "react";
+import api from "./axios/axios"
 
 const HomeScreen = () => {
+    const [stateLED, setStateLED] = useState({value: false});
     const navigation = useNavigation();
     const handleCam= () => {
         navigation.navigate("Cam");
     }
 
+    async function ledToggle(){
+        try{
+            //Faz a requisição da API adafruit
+            const response = await api.toggleLED({value: `${!stateLED.value}`});
+            setStateLED({value: !stateLED.value})
+            console.log("resposta: ", response.data);
+        }
+        catch(error){
+            console.log("Erro", error.response.data);
+        }
+    }
+
     return(
         <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <View style={{marginBottom: 20}}>
             <Button title="Abrir camera" onPress={handleCam} color='blue'/>
+            </View>
+            <View>
+                <Button title={stateLED.value ? "Desligar LED" : "Ligar LED"} onPress={ledToggle} color={stateLED.value ? "red" : "green"}/>
+            </View>
         </View>
     )
 
